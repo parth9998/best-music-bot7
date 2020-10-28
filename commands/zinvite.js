c4e2b34e133b6e6bb3beb0904bbc74f57915b410
parth9998 <67903668+parth9998@users.noreply.github.com>
2020-10-28 18:48:01 +0530

    Update zinvite.js

--- commands/zinvite.js
+++ commands/zinvite.js
@@ -2,8 +2,8 @@ const { MessageEmbed } = require("discord.js");
 
 module.exports = {
  
-   // name: "invite",
-    //description: "Add bot to your Server https://discord.com/oauth2/authorize?client_id=770244201102704690&permissions=37080128&scope=bot ",
+    name: "invite",
+    description: "Add bot to your Server https://discord.com/oauth2/authorize?client_id=770244201102704690&permissions=37080128&scope=bot ",
       
   }
 
