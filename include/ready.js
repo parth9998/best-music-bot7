f1745650cac53679c46aef9f1833f664d4db1476
parth9998 <67903668+parth9998@users.noreply.github.com>
2020-12-01 11:31:13 +0530

    Update ready.js

--- include/ready.js
+++ include/ready.js
@@ -2,7 +2,7 @@
 
 module.exports = async client => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("VENOM --help",{
+  await client.user.setActivity("XD //help",{
     type: "LISTENING" //can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
